068f43d8ed7ad7a818b5641de35c3c1c12a502f8
SirShinHub <93308719+SirShinHub@users.noreply.github.com>
2022-12-26 03:34:25 +0700

    Update Dragonaworlds.lua

--- Dragonaworlds.lua
+++ Dragonaworlds.lua
@@ -25,6 +25,6 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/SirShinHub/SirShinHub
 elseif game.PlaceId == 4728805070 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SirShinHub/SirShinHub/main/Dragonadventures.lua'))()   
 elseif game.PlaceId == 5777228223 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SirShinHub/SirShinHub/main/Dragonadventures.lua'))()   
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/Dragonadventures'))()   
 
 end
