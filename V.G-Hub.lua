44734a60a3aef778bb0b1e0c9825599a5576ea2b
SirShinHub <93308719+SirShinHub@users.noreply.github.com>
2023-06-30 01:54:32 +0700

    Update V.G-Hub.lua

--- V.G-Hub.lua
+++ V.G-Hub.lua
@@ -2,7 +2,7 @@ repeat
     wait()
 until game:IsLoaded()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extras/main/Universal_Client_Bypass"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SirShinHub/SirShinHub/main/Universal_Client_Bypass.lua"))()
 local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/SirShinHub/SirShinHub/main/V.G-Hub-DA-LIST.lua"))()
 getgenv().Get =
     setmetatable(
