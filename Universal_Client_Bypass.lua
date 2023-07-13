84f0b638bd59c4afaca1935adcd9c2b4e6d8fcf1
SirShinHub <93308719+SirShinHub@users.noreply.github.com>
2023-07-14 04:25:10 +0700

    Update Universal_Client_Bypass.lua

--- Universal_Client_Bypass.lua
+++ Universal_Client_Bypass.lua
@@ -1,13 +1,13 @@
-repeat task.wait() until game:IsLoaded()
+repeat wait() until game:IsLoaded()
 local G = getgenv and getgenv() or _G or shared
-if game.PlaceId == 185655149 then
-    wait(50)
+G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
+
+for i,v in pairs(game.GetChildren(game)) do
+    G[v.ClassName] = v
 end
-for i,v in pairs(game:GetChildren()) do
-    G[v.ClassName] = v 
-end 
+
 G.Player = Players.LocalPlayer
-G.wait = task.wait 
+G.wait = task.wait
 G.spawn = task.spawn
 G.Heartbeat = RunService.Heartbeat
 G.Stepped = RunService.Stepped
@@ -16,62 +16,64 @@ G.Error = ScriptContext.Error
 G.MessageOut = LogService.MessageOut
 G.Kick = Player.Kick
 G.Idled = Player.Idled
-G.GC = GetChildren
-G.GD = GetDescendants
 
 local Name = game.PlaceId .. ".json"
 local Des = {}
 if makefolder and not isfile("V.G Hub") then
     makefolder("V.G Hub")
-end 
+end
 
 G.Settings = {}
 local Pcall = pcall(function()
     if isfile("V.G Hub//" .. Name) then
         readfile("V.G Hub//" .. Name)
-    else 
+    else
         writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Des))
-    end 
+    end
 end)
 
 if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
     Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
-end 
+end
 
 
 local Nos = {
     "PreloadAsync",
-    "Kick",
-     "kick",
     "xpcall",
     "gcinfo",
     "collectgarbage",
     "FindService",
 }
+
+local Yes = {
+    "Kick",
+    "kick",
+}
+
 local Disables = {
     Error,
-    MessageOut
+    MessageOut,
+    Idled
 }
+
 
 local OldNameCall = nil
 OldNameCall = hookmetamethod(game, "__namecall", function(...)
     local Args = {...}
     local A, B, C = ...
-    if table.find(Nos, A.Name) then
-        return 
-    end 
+    if table.find(Yes, getnamecallmethod()) and A == Player then
+        return
+    end
+    if table.find(Nos, getnamecallmethod()) then
+        return
+    end
+    if type(A) ~= "Instance" then
+        return OldNameCall(...)
+    end
     return OldNameCall(...)
 end)
 
-for i,v in pairs(Disables) do 
-    for i,v in pairs(getconnections(v)) do
-        if v.Disable then
-            v:Disable()
-        elseif v.Disconnect then
-            v:Disconnect()
-        end 
-    end 
-end
+
 if setfflag then
     setfflag("HumanoidParallelRemoveNoPhysics", "False")
     setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
@@ -80,44 +82,97 @@ if setfpscap then
     setfpscap(100)
 end
 
-ScriptContext:SetTimeout(0.5)
-
 G.GetFunction = function(A)
-    for i,v in pairs(getgc()) do
-        if type(v) == "function" and getinfo(v).name == tostring(A) then
-            A = v 
-        end 
-    end 
-    return A 
-end 
+    for i,v in next, getgc() do
+        if type(v) == "function" and getinfo(v).name == A and islclosure(v) then
+            return v 
+        end
+    end
+end
 
 G.Teleport = function(A, B, Toggle)
     if Toggle and A and B then
-        A.CFrame = B.CFrame 
-    end 
+        A.CFrame = B
+    end
     return A, B, Toggle
-end 
+end
 
-G.DisableConnection = function(A, B)
-    for i,v in pairs(getconnections(A[tostring(B)])) do 
+G.DisableConnection = function(A)
+    for i,v in next, getconnections(A) do 
         v:Disable()
-    end 
-    return A, B 
+    end
+    return A
+end
+
+G.FireConnection = function(A)
+    for i,v in next, getconnections(A) do
+        v:Fire()
+    end
 end
 
 G.Tween = function(A, B, C)
     if A and B then
         local Time = (B.Position - A.Position).Magnitude / C 
         local Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
-        local Tween = TweenService(A, Info, {CFrame = CFrame.new(B.Position)})
+        local Tween = TweenService:Create(A, Info, {CFrame = CFrame.new(B.Position)})
         Tween:Play()
-        wait(Time)
-    end 
+        if Tween.Completed then
+            Tween.Completed:Wait()
+        end
+    end
     return A, B, C
-end 
+end
 
 G.Save = function()
     pcall(function()
         writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Settings))
     end)
-end 
+end
+
+G.ServerHop = function()
+    spawn(function()
+        while wait() do
+            pcall(function()
+                local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+                for i,v in next, Gay.data do
+                    if v.playing < v.maxPlayers then
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
+                        break
+                    end
+                end
+            end)
+            wait(4)
+        end
+    end)
+end
+
+G.Rejoin = function()
+    return TeleportService:Teleport(game.PlaceId, Player)
+end
+
+G.NoClip = function(A)
+    return A:ChangeState(11)
+end
+G.NoClip2 = function(A)
+    for i,v in next, A:GetChildren() do
+        if v:IsA("BasePart") then
+            v.CanCollide = false 
+        end
+    end
+end
+G.SendNotification = function(Title, Text, Icon, Duration)
+    return StarterGui:SetCore("SendNotification", {Title = Title, Text = Text, Icon = Icon, Duration = Duration})
+end
+
+G.Mag = function(A, B)
+    return (A.Position - B.Position).Magnitude
+end
+
+for i,v in next, Disables do 
+    for i,v in next, getconnections(v) do
+        v:Disable()
+    end
+end
+
+ScriptContext:SetTimeout(0)
+local getconstants=debug.getconstants or getconstants;local getidentity=get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity;local setidentity=set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity;local hookfunc=hookfunction or hookfunc or detour_function;for a,b in next,getgc()do if type(b)=="function"and islclosure(b)then local c=getconstants(b)if table.find(c,"Detected")and table.find(c,"crash")then hookfunc(b,function()return task.wait(math.huge)end)end end end
